93cda5f86ab97a4028f09c354225b8f536a67ba2
Omar Jabbour <128647309+omaarjb@users.noreply.github.com>
2025-03-20 21:14:05 +0000

    fill the spinner

--- app/src/main/java/com/example/tp/MainActivity.java
+++ app/src/main/java/com/example/tp/MainActivity.java
@@ -2,6 +2,11 @@ package com.example.tp;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +15,12 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText number;
+    private Spinner input,output;
+    private Button convertButton;
+    private TextView result;
+
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -22,5 +33,17 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        number = findViewById(R.id.idNumber);
+        input=findViewById(R.id.idInput);
+        output=findViewById(R.id.idOutput);
+        convertButton=findViewById(R.id.convertButton);
+
+        String[] bases = {"Décimal", "Binaire", "Octal", "Hexadécimal"};
+        ArrayAdapter<String> adapter=new ArrayAdapter<>(this, android.R.layout.simple_spinner_item,bases);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        input.setAdapter(adapter);
+        output.setAdapter(adapter);
+
     }
 }
